bf4ea984183f8dac2000a80588cecb47a2c1b0b1
azivkovi <zivkovic.ante@gmail.com>
2023-12-18 15:24:01 +0100

    fix

--- src/actions/send-command.action.js
+++ src/actions/send-command.action.js
@@ -4,10 +4,11 @@ import { redirect } from 'next/navigation';
 
 const sendCommand = async (command) => {
   try {
+    //INFO: Tu ide api call prema Apache MQ serveru
     const AMQ_URL = 'http://localhost:8161/api/message/test?type=queue';
     const AMQ_USERNAME = 'admin';
     const AMQ_PASSWORD = 'admin';
-    await fetch(AMQ_URL, {
+    const res = await fetch(AMQ_URL, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded',
@@ -15,9 +16,8 @@ const sendCommand = async (command) => {
       },
       body: `body=${command}`,
     });
-    // const data = await res.json();
+    const data = await res.json();
   } catch (error) {
-    console.log(error);
     redirect('/?res=error');
   }
 
